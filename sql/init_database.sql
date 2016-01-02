477ab0f45e3525eb7b55cef8308b5e2846ec3fc0
antoine <antoine@cormery.eu>
2016-01-02 04:55:48 +0100

    fix sql

--- sql/init_database.sql
+++ sql/init_database.sql
@@ -1,5 +1,15 @@
-\h
-exit
-\q
-\q
-\s init_database.sql
+DROP TABLE IF EXISTS account;
+
+CREATE TABLE account (id SERIAL PRIMARY KEY, pseudo TEXT, mail TEXT);
+
+CREATE OR REPLACE FUNCTION ADD_ACCOUNT(PSEUDO TEXT, MAIL TEXT) RETURNS boolean AS
+$$
+BEGIN
+    IF NOT EXISTS (SELECT * FROM account WHERE account.pseudo = ADD_ACCOUNT.PSEUDO) THEN
+        INSERT INTO account (pseudo, mail) VALUES (ADD_ACCOUNT.PSEUDO, ADD_ACCOUNT.MAIL);
+        RETURN FOUND;
+    END IF;
+    RETURN false;
+END;
+$$ LANGUAGE plpgsql;
+
